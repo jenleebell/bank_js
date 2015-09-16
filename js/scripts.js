fba0a6583241fd8d15a8307ce466d72abd60cd92
Jennifer <jenleebell@gmail.com>
2015-09-15 19:30:23 -0700

    Add partial code for withdraw functionality

--- js/scripts.js
+++ js/scripts.js
@@ -15,12 +15,21 @@ $(document).ready(function() {
 	$("form#create-account").submit(function(event){
 		var name = $("input#name").val();
 		var balance = $("input#balance").val();
-		var newAccount = new Account(name, balance);
-			var newName = newAccount.name;
-			var newBalance = newAccount.balance;
+		var userAccount = new Account(name, balance);
+			var newName = userAccount.name;
+			var newBalance = userAccount.balance;
 
 		$(".account-holder").text("Welcome " + newName + " :: Your Balance is " + "$" + newBalance);
 		$("#account-balance").show();
 		event.preventDefault();
+
+	$("form#make-deposit").submit(function(event){
+		var newDeposit = $("input#deposit").val();
+		var newAddedBalance = userAccount.deposit(newDeposit);
+
+		$(".account-holder").text("Welcome " + newName + " :: Your Balance is " + "$" + newAddedBalance);
+		$("#account-balance").show();
+		event.preventDefault();
+		});
 	});
 });
